2ffeb400f9c617fd524edb15ecc324da0b141730
akyboy9 <ivatury.akshay@gmail.com>
2018-01-25 15:41:29 -0600

    inital blockchain skeleton

---
+++ blockchain.py
@@ -0,0 +1,20 @@
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.transactoins = []
+
+
+    def new_block(self):
+        pass
+
+    def new_transaction(self):
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    @property
+    def last_block(self):
+        pass
+
